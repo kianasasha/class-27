a12cd6c472380ff288e1bf0cd85862e6582c47e0
kianasasha <82643028+kianasasha@users.noreply.github.com>
2021-07-24 13:43:08 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,21 @@
+class Slingshot{
+    constructor(bodyA,bodyB){
+        var options={
+            bodyA: bodyA,
+            bodyB: bodyB,
+            length:10,
+            stiffness: 0.1 
+            }
+            this.sling = Constraint.create(options);
+            World.add(world,this.sling);
+
+    }
+    display(){
+        var posA=this.sling.bodyA.position;
+        var posB=this.sling.bodyB.position;
+        push();
+        strokeWeight(7)
+        line(posA.x,posA.y,posB.x,posB.y);
+        pop();
+    }
+}
